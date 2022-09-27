1bf018b6ef76512cff72e99d99df469300594f2d
giorgimeladze <giorgi_meladze@epam.com>
2022-09-27 13:14:28 +0400

    Moved whole output details to HashOutputGenerator

--- lib/generators/hash_output_generator.rb
+++ lib/generators/hash_output_generator.rb
@@ -4,6 +4,7 @@ require_relative 'sorted_hash_generator'
 
 class HashOutputGenerator
   attr_reader :hash
+
   def initialize(hash)
     @hash = hash
   end
@@ -11,12 +12,31 @@ class HashOutputGenerator
   def generate(sorting_generator)
     if hash.key?(:message)
       puts hash[:message]
-    else
-      puts 'Output of most visits on a page'
-      sorting_generator.generate_visits(hash)
-      puts 'Output of the most uniq visits on  a page'
-      sorting_generator.generate_uniq_visits(hash)
-      puts 'Thanks for your time'
+      return
+    end
+
+    puts 'Output of most visits on a page'
+    visits_hash = sorting_generator.generate_visits(hash)
+    output_visits_hash_context(visits_hash)
+
+    puts 'Output of the most uniq visits on  a page'
+    uniq_visits_hash = sorting_generator.generate_uniq_visits(hash)
+    output_uniq_visits_hash_context(uniq_visits_hash)
+
+    puts 'Thanks for your time'
+  end
+
+  private
+
+  def output_visits_hash_context(visits_hash)
+    visits_hash.each do |uri, uri_value|
+      printf "%-30s %d \n", "#{uri} visits", uri_value[:visits].size
+    end
+  end
+
+  def output_uniq_visits_hash_context(uniq_visits_hash)
+    uniq_visits_hash.each do |uri, uri_value|
+      printf "%-30s %d \n", "#{uri} unique visits", uri_value[:uniq_visits].size
     end
   end
 end
